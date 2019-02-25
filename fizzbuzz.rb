e4bf188d6d7789dd518dcfbc05bc0a3a6680a0c5
Mario Adamo Jr <marioadamojr@gmail.com>
2019-02-25 02:26:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(x)
-  if x == 3 % 0 && x == 5 % 0
+  if x % 3 == 0 && x % 5 == 0
     "Fizzbuzz"
-  elsif x == 3 % 0
+  elsif x % 3 == 0
     "Fizz"
-  elsif x == 5 % 0
+  elsif x % 5 == 0
     "Buzz"
   else
     nil
